dd5feff6139d02e05cc1dab5f667c814068ca14a
boduill <acseler@mail.ru>
2016-01-19 11:38:21 +0300

    Changed FriendDao

---
+++ src/test/java/com/universe/DAO/DAOLayer/DialogDAOTest.java
@@ -0,0 +1,11 @@
+package com.universe.DAO.DAOLayer;
+
+import junit.framework.TestCase;
+
+public class DialogDAOTest extends TestCase {
+
+
+    public void testGetDialogs() throws Exception {
+
+    }
+}
